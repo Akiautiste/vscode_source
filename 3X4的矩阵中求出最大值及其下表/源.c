5ebcb10870a07dab807007f5966291d574e4f87c
Aki_autiste <cuiqiang988@gmail.com>
2020-06-09 22:31:32 +0800

    evening

--- 3X4的矩阵中求出最大值及其下表/源.c
+++ 3X4的矩阵中求出最大值及其下表/源.c
@@ -28,3 +28,27 @@ void main()
 	else
 		printf("max number is %d,row is 1,colum 1\n", a[0][0]);
 }
+/*
+void main()
+{
+	int i, j, row = 0, colum = 0, max;
+	int a[3][4] = { {1,2,3,4},{9,8,7,6},{-10,10,-5,2} };
+	max = a[0][0];
+	for ( i = 0; i <= 2; i++)
+	{
+		for ( j = 0; j <= 3; j++)
+		{
+			if (a[i][j] > max)
+			{
+				max = a[i][j];
+				row = i;
+				colum = j;
+
+			}
+		}
+		
+	}
+	printf("max=%d,row=%d,colum=%d\n", max, row, colum);
+	printf("\n:");
+}
+*/

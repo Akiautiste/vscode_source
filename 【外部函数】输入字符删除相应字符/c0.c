ac7d342ea50bbb20d9f110380db0dea4e3c53e8b
Aki_autiste <cuiqiang988@gmail.com>
2020-06-17 23:12:44 +0800

    2020_6_17

--- 【外部函数】输入字符删除相应字符/c0.c
+++ 【外部函数】输入字符删除相应字符/c0.c
@@ -4,7 +4,7 @@ void main()
 {
 	extern void enter_string(char str[]);
 	extern void delete_string(char str[], char sh);
-	extern void print_string(char str[]);
+    extern void print_string(char str[]);
 	char c;
 	char str[80];
 	enter_string(str);
